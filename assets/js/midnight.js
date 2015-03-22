72673ce7d5072efccdf841cc2c09e53e660dccfe
xa0s <xa0s@users.noreply.github.com>
2015-03-22 03:52:37 +0200

    Subscribe in popup

--- assets/js/midnight.js
+++ assets/js/midnight.js
@@ -9,12 +9,16 @@ function madmimiDigestCallback(result) {
 	$(document).ready(wire);
 
 	function wire() {
-		$("form.madmimi.digest").submit(MadMimi.subscribeDigest);
+		$('form.madmimi.digest').submit(MadMimi.subscribeDigest);
+		$('.subscribe-up').click(SubscribePanel.show);
+		$('.subscribe-down').click(SubscribePanel.hide);
 	}
 
 	var MadMimi = {
 		subscribeDigest: function (event) {
 			event.preventDefault();
+
+			SubscribePanel.hide();
 
 			var form = $(event.target);
 			var id = form.data('madmimi-id');
@@ -24,6 +28,38 @@ function madmimiDigestCallback(result) {
 			var script = document.createElement('script'); script.type = 'text/javascript'; script.async = true;
 			script.src = 'https://madmimi.com/signups/subscribe/' + id + '.json?callback=madmimiDigestCallback&signup[email]=' + email;
 			document.body.appendChild(script);
+		}
+	}
+
+	var SubscribePanel = {
+		show: function () {
+			var panel = $('#subscribe');
+			panel.show();
+			panel.animate(
+				{ 
+					'height': '60%' 
+				}, 
+				{ 
+					'duration': 500,
+					'done': function(){
+						$('#subscribe input').focus();
+					}
+				}
+			);
+		},
+		hide: function () {
+			var panel = $('#subscribe');
+			panel.animate(
+				{ 
+					'height': 0 
+				}, 
+				{ 
+					'duration': 500,
+					'done': function(){
+						panel.hide()
+					}
+				}
+			);
 		}
 	}
 
