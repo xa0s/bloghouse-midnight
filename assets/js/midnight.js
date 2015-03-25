f5445d9d79e1e62eea842d2bb15b647d3e20d68e
xa0s <xa0s@users.noreply.github.com>
2015-03-25 02:44:16 +0200

    Save more info on user subscription

--- assets/js/midnight.js
+++ assets/js/midnight.js
@@ -14,15 +14,20 @@ var midnight = (function() {
 		subscribeDigest: function (event) {
 			event.preventDefault();
 
-			SubscribePanel.hide()
+			SubscribePanel.hide();
 
 			var form = $(event.target);
 			var id = form.data('madmimi-id');
 			var lang = form.data('lang');
 			var email = form.find('input.email').first().val();
+			var type = "news";
 
 			var script = document.createElement('script'); script.type = 'text/javascript'; script.async = true;
-			script.src = 'https://madmimi.com/signups/subscribe/' + id + '.json?callback=midnight.madmimi.callback.digest&signup[email]=' + email;
+			script.src = 'https://madmimi.com/signups/subscribe/' + id + '.json?callback=midnight.madmimi.callback.digest' 
+				+ '&signup[email]=' + email
+				+ '&signup[lang]=' + lang
+				+ '&signup[type]=' + type
+				;
 			document.body.appendChild(script);
 		},
 		callback: {
